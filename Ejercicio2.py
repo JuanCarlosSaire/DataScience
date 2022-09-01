c67d81e015769e8b0d6b8564d6a494cb38f25f82
JuanCarlosSaire <juancarlossaire@yahoo.es>
2022-09-01 13:48:33 +0200

    Subida del Ejericicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,48 @@
+# EJERCICIO 1
+
+"""
+   Dado el siguiente listado: ["Python", "Matlab", "R", "VBA", "Julia", "C++"].
+
+    Modifica con un algoritmo ese listado.
+
+    Cuando encuentre Python debe poner un 1
+    y cuando encuentre otro lenguaje de programacion, un 0
+    es un simple ejemplo de modificación de valores en una lista.
+"""
+
+# EJERCICIO 2
+
+"""
+    Dada la siguiente lista:
+    L = [10, None, 8, 5, None, 20]
+"""
+    # 1) Susitituir por -1 el valor None usando bucles y listas
+    # 2) Creamos un dataframe con los valores de la lista y
+    #     modificamos los "NaN" por un valor de -1 (Valores nulos, suma, etc..)
+    # 3) Vuelve a escribir el listado con falta de valores (inicial),
+    #     y sustituye por la media.
+    # 4) Apendiza la columna con estos valores
+    #     listado2 = [10, 20, 50, 30, 20, 0]
+    # 5) Elimina la columna L
+
+
+# EJERCICIO 3
+
+"""
+    Crear un listado con los siguientes numeros:
+        10, 20, 30, 40 (nombra la lista con nombre: "listado")
+"""
+    # 1) Crea el listado e imprimelo:
+    # 2) Apendiza el valor 50 ( si es posible)
+    # 3) Modifica (si es posible) el valor 10 por 100
+
+
+# EJERCICIO 4
+
+"""
+    Da una lista de nombre "Temperatura" con valores: 10, 20, 30, 40, 50
+"""
+    # 1) Crea el listado e imprimelo:
+    # 2) En este "Temperatura". ¿Cuál es el elemento en la posición (index) 1?
+    # 3) ¿Y en la posición (index) 0?
+    # 4) ¿Y en la posición (index) -1?
